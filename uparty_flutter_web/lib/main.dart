fc097cd6d24a5a3c3e73ae6400ad2a9289fee628
Ben Dempsey <bndempsey@gmail.com>
2020-01-21 10:02:10 -0600

    Stuff added to contact tab

--- uparty_flutter_web/lib/main.dart
+++ uparty_flutter_web/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:uparty_flutter_web/vertical_navigation_bar_clone.dart';
 import 'responsive_builder_clone.dart';
 import 'shifting_tabbar_clone.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -11,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'uParty',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'uParty'),
     );
   }
 }
@@ -67,7 +66,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
     final navItems = [
       SideNavigationItem(iconData: Icons.home, title: "Home"),
-      SideNavigationItem(iconData: Icons.question_answer, title: "About"),
+      SideNavigationItem(iconData: Icons.library_books, title: "About"),
+      SideNavigationItem(iconData: Icons.contacts, title: "Contact Us"),
       SideNavigationItem(iconData: Icons.gavel, title: "Legal"),
     ];
     final initialTab = 0;
@@ -79,6 +79,11 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return ResponsiveBuilder(builder: (context, sizingInformation) {
+      //constants for pages
+      const int HOME = 0;
+      const int ABOUT = 1;
+      const int CONTACT = 2;
+      const int LEGAL = 3;
       // Check the sizing information here and return your UI
       if (sizingInformation.deviceScreenType == DeviceScreenType.Desktop ||
           sizingInformation.deviceScreenType == DeviceScreenType.Tablet) {
@@ -99,16 +104,161 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               Expanded(
                 child: PageView.builder(
-                  itemCount: 3,
+                  itemCount: 4,
                   controller: pageController,
                   scrollDirection: Axis.vertical,
                   physics: NeverScrollableScrollPhysics(),
                   itemBuilder: (context, index) {
-                    return Container(
-                        color: Colors.black.withOpacity(0.8),
-                        child: Center(
-                          child: Text("Page " + index.toString()),
-                        ));
+                    switch (index) {
+                      case HOME:
+                        {
+                          return Container(
+                              color: Colors.black.withOpacity(0.8),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Text(
+                                          "Title",
+
+                                          //textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                              fontSize: 32,
+                                              color: Colors.white),
+                                        ),
+                                        Text(
+                                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit,\nsed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              color: Colors.white),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.all(16.0),
+                                          child: RaisedButton(
+                                            color: Colors.blue,
+                                            child: Text(
+                                              "Sign Up",
+                                              style: TextStyle(fontSize: 20),
+                                            ),
+                                            onPressed: () {},
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Container(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.4,
+                                          child:
+                                              Image.asset('assets/ducks.png'),
+                                        )
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ));
+                          // Container(
+                          //   color: Colors.red,
+                          //   child: Row(
+                          //     mainAxisSize: MainAxisSize.max,
+                          //     children: <Widget>[
+                          //       Column(
+                          //         children: <Widget>[Text("FUCK")],
+                          //       ),
+                          //       Column(
+                          //         children: <Widget>[Text("YOU")],
+                          //       )
+                          //     ],
+                          //   ),
+                          // );
+                        }
+                        break;
+                      case ABOUT:
+                        {
+                          return Container(
+                            color: Colors.black.withOpacity(0.8),
+                            child: ListView(children: <Widget>[
+                              Center(
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(16, 64, 16, 32),
+                                  child: Text("About Title",
+                                      style: TextStyle(
+                                          fontSize: 40, color: Colors.white),
+                                      textAlign: TextAlign.center),
+                                ),
+                              ),
+                              Center(
+                                child: Text(
+                                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit,\nsed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 28, color: Colors.white),
+                                ),
+                              ),
+                            ]),
+                          );
+                        }
+                        break;
+                      case CONTACT:
+                        {
+                          return Container(
+                            color: Colors.black.withOpacity(0.8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                _createDesktopContact(
+                                    "Emmett Duck",
+                                    "Chief Executive Officer",
+                                    "duck",
+                                    "duck",
+                                    "duck",
+                                    "duck"),
+                                _createDesktopContact(
+                                    "Ben Ducky",
+                                    "Chief Technology Officer",
+                                    "duck",
+                                    "duck",
+                                    "duck",
+                                    "duck"),
+                                _createDesktopContact(
+                                    "Flyin Andress",
+                                    "Chief Marketing Officer",
+                                    "duck",
+                                    "duck",
+                                    "duck",
+                                    "duck"),
+                                _createDesktopContact(
+                                    "Duckling Carrol",
+                                    "Marketing Director",
+                                    "duck",
+                                    "duck",
+                                    "duck",
+                                    "duck"),
+                              ],
+                            ),
+                          );
+                        }
+                        break;
+                    }
+                    // return Container(
+                    //     color: Colors.black.withOpacity(0.8),
+                    //     child: Center(
+                    //       child: Text("Page " + index.toString()),
+                    //     ));
                   },
                 ),
               )
@@ -119,7 +269,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
       if (sizingInformation.deviceScreenType == DeviceScreenType.Mobile) {
         return DefaultTabController(
-          length: 3,
+          length: 4,
           child: Scaffold(
             appBar: ShiftingTabBar(
               color: Colors.black87,
@@ -128,31 +278,185 @@ class _MyHomePageState extends State<MyHomePage> {
                   icon: Icon(Icons.home),
                   text: "Home",
                 ),
-                ShiftingTab(
-                  icon: Icon(Icons.question_answer),
-                  text: "About"
-                ),
-                ShiftingTab(
-                  icon: Icon(Icons.gavel),
-                  text: "Legal"
-                )
+                ShiftingTab(icon: Icon(Icons.library_books), text: "About"),
+                ShiftingTab(icon: Icon(Icons.contacts), text: "Contact"),
+                ShiftingTab(icon: Icon(Icons.gavel), text: "Legal")
               ],
-              
             ),
             body: TabBarView(
               children: <Widget>[
-                Icon(Icons.home),
-                Icon(Icons.question_answer),
+                Container(
+                  color: Colors.black.withOpacity(0.8),
+                  child: ListView(
+                    children: <Widget>[
+                      Center(
+                          child: Padding(
+                        padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+                        child: Text(
+                          "Title",
+                          style: TextStyle(fontSize: 32, color: Colors.white),
+                        ),
+                      )),
+                      Center(
+                        child: Text(
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit,\nsed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ),
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: RaisedButton(
+                            color: Colors.blue,
+                            child: Text(
+                              "Sign Up",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.6,
+                          child: Image.asset('assets/ducks.png'),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.black.withOpacity(0.8),
+                  child: ListView(children: <Widget>[
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(16, 32, 16, 32),
+                        child: Text("About Title",
+                            style: TextStyle(fontSize: 32, color: Colors.white),
+                            textAlign: TextAlign.center),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit,\nsed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                  ]),
+                ),
+                Container(
+                  child: Center(
+                    child: ListView(
+                      children: <Widget>[
+                        _createMobileContact(
+                            "Emmett Duck",
+                            "Chief Executive Officer",
+                            "duck",
+                            "duck",
+                            "duck",
+                            "duck"),
+                        _createMobileContact(
+                            "Ben Ducky",
+                            "Chief Technology Officer",
+                            "duck",
+                            "duck",
+                            "duck",
+                            "duck"),
+                        _createMobileContact(
+                            "Flyin Andress",
+                            "Chief Marketing Officer",
+                            "duck",
+                            "duck",
+                            "duck",
+                            "duck"),
+                        _createMobileContact("Duckling Carrol",
+                            "Marketing Director", "duck", "duck", "duck", "duck"),
+                      ],
+                    ),
+                  ),
+                ),
                 Icon(Icons.gavel)
               ],
             ),
           ),
-          
         );
       }
 
       return Container(color: Colors.purple);
     });
-    
+  }
+
+  Widget _createDesktopContact(String name, String title, String imgPath,
+      String linkedIn, String email, String instagram) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Container(
+              child: Image.asset('assets/ducks.png'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 0, 16, 32),
+            child: Text(
+              name,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+              ),
+            ),
+          ),
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _createMobileContact(String name,String title,String imgPath, String linkedIn, String email, String instagram){
+    return Expanded(
+      child: Row(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Container(
+                  child: Image.asset('assets/ducks.png'),
+                ),
+                Padding(
+                padding: const EdgeInsets.fromLTRB(16, 0, 16, 32),
+                child: Text(
+                  name,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                  ),
+                ),
+              ),
+              Text(
+                title,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
